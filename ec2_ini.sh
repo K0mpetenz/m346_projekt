f2885e7fb73c0692bcdf3d5e8c8977e5962e2619
Ben Kaelin <Ben.Kaelin@edu.gbssg.ch>
2025-12-16 23:43:36 +0100

    BKA: bugfix rework

--- ec2_ini.sh
+++ ec2_ini.sh
@@ -1,114 +1,118 @@
-## EC2-Nextcloud-Instance
+#!/bin/bash
+set -e
 
-# define key pair name
-NC_KEY_NAME="nextcloud-key"
+### ======================
+### VARIABLEN
+### ======================
+AMI_ID="ami-08c40ec9ead489470"
+INSTANCE_TYPE="t2.micro"
+IAM_PROFILE="LabInstanceProfile"
 
-# define instance name
-NC_INSTANCE_NAME="NC-Server"
+NC_KEY_NAME="nextcloud-key_test"
+DB_KEY_NAME="database-key_test"
 
-# define security group
-NC_SECURITY_GROUP="nc-sec-group"
+NC_INSTANCE_NAME="NC-Server_test"
+DB_INSTANCE_NAME="DB-Server_test"
+
+### ======================
+### KEYPAIRS
+### ======================
+aws ec2 create-key-pair \
+  --key-name "$NC_KEY_NAME" \
+  --query 'KeyMaterial' \
+  --output text > ~/.ssh/$NC_KEY_NAME.pem
 
 aws ec2 create-key-pair \
-    --key-name "$NC_KEY_NAME" \
-    --key-type rsa \
-    --query 'KeyMaterial' \
-    --output text > ~/.ssh/"$NC_KEY_NAME".pem
+  --key-name "$DB_KEY_NAME" \
+  --query 'KeyMaterial' \
+  --output text > ~/.ssh/$DB_KEY_NAME.pem
 
-aws ec2 create-security-group \
-    --group-name "$NC_SECURITY_GROUP" \
-    --description "$NC_INSTANCE_NAME"
+chmod 600 ~/.ssh/*.pem
+
+### ======================
+### SECURITY GROUPS
+### ======================
+
+# Nextcloud SG
+NC_SG_ID=$(aws ec2 create-security-group \
+  --group-name nc-sec-group \
+  --description "Nextcloud SG" \
+  --query GroupId \
+  --output text)
 
 aws ec2 authorize-security-group-ingress \
-    --group-name "$NC_SECURITY_GROUP" \
-    --protocol tcp --port 80 --cidr 0.0.0.0/0
+  --group-id $NC_SG_ID \
+  --protocol tcp --port 22 --cidr 0.0.0.0/0
 
 aws ec2 authorize-security-group-ingress \
-    --group-name "$NC_SECURITY_GROUP" \
-    --protocol tcp --port 22 --cidr 0.0.0.0/0
-aws ec2 run-instances \
-    --image-id ami-08c40ec9ead489470 \
-    --count 1 \
-    --instance-type t2.micro \
-    --key-name "$NC_KEY_NAME" \
-    --security-groups "$NC_SECURITY_GROUP" \
-    --iam-instance-profile Name=LabInstanceProfile \
-    --user-data file://nc_ini.txt \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NC_INSTANCE_NAME}]" > /dev/null
+  --group-id $NC_SG_ID \
+  --protocol tcp --port 80 --cidr 0.0.0.0/0
 
-# change the permissions for the ssh key to 600
-chmod 600 ~/.ssh/"$NC_KEY_NAME".pem
+# DB SG
+DB_SG_ID=$(aws ec2 create-security-group \
+  --group-name db-sec-group \
+  --description "DB SG" \
+  --query GroupId \
+  --output text)
 
-# get the nextcloud instance id
-NC_INSTANCE_ID=$(aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$NC_INSTANCE_NAME" \
-    --query "Reservations[0].Instances[0].InstanceId" \
-    --output text)
+aws ec2 authorize-security-group-ingress \
+  --group-id $DB_SG_ID \
+  --protocol tcp --port 22 --cidr 0.0.0.0/0
 
-# get the nextcloud instance public ip
+# MySQL NUR vom Nextcloud-SG
+aws ec2 authorize-security-group-ingress \
+  --group-id $DB_SG_ID \
+  --protocol tcp --port 3306 \
+  --source-group $NC_SG_ID
+
+### ======================
+### EC2 INSTANZEN
+### ======================
+
+# Nextcloud
+NC_INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $NC_KEY_NAME \
+  --security-group-ids $NC_SG_ID \
+  --iam-instance-profile Name=$IAM_PROFILE \
+  --user-data file://nc_ini.yml \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$NC_INSTANCE_NAME}]" \
+  --query "Instances[0].InstanceId" \
+  --output text)
+
+# Database
+DB_INSTANCE_ID=$(aws ec2 run-instances \
+  --image-id $AMI_ID \
+  --instance-type $INSTANCE_TYPE \
+  --key-name $DB_KEY_NAME \
+  --security-group-ids $DB_SG_ID \
+  --iam-instance-profile Name=$IAM_PROFILE \
+  --user-data file://db_ini.yml \
+  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]" \
+  --query "Instances[0].InstanceId" \
+  --output text)
+
+### ======================
+### INFOS AUSGEBEN
+### ======================
+
+sleep 10
+
 NC_PUBLIC_IP=$(aws ec2 describe-instances \
-    --instance-ids "$NC_INSTANCE_ID" \
-    --query "Reservations[0].Instances[0].PublicIpAddress" \
-    --output text)
+  --instance-ids $NC_INSTANCE_ID \
+  --query "Reservations[0].Instances[0].PublicIpAddress" \
+  --output text)
 
-# print out the public ip
-echo -e "Erreiche Nextcloud unter https://$NC_PUBLIC_IP"
+DB_PRIVATE_IP=$(aws ec2 describe-instances \
+  --instance-ids $DB_INSTANCE_ID \
+  --query "Reservations[0].Instances[0].PrivateIpAddress" \
+  --output text)
 
-
-#----#
-
-## EC2-Database-Instance
-
-# define key pair name
-DB_KEY_NAME="database-key"
-
-# define instance name
-DB_INSTANCE_NAME="DB-Server1"
-
-# define security group
-DB_SECURITY_GROUP="db-sec-group"
-
-aws ec2 create-key-pair \
-    --key-name "$DB_KEY_NAME" \
-    --key-type rsa \
-    --query 'KeyMaterial' \
-    --output text > ~/.ssh/"$DB_KEY_NAME".pem
-
-aws ec2 create-security-group \
-    --group-name "$DB_SECURITY_GROUP" \
-    --description "$DB_INSTANCE_NAME"
-
-aws ec2 authorize-security-group-ingress \
-    --group-name "$DB_SECURITY_GROUP" \
-    --protocol tcp --port 80 --cidr 0.0.0.0/0
-
-aws ec2 authorize-security-group-ingress \
-    --group-name "$DB_SECURITY_GROUP" \
-    --protocol tcp --port 22 --cidr 0.0.0.0/0
-aws ec2 run-instances \
-    --image-id ami-08c40ec9ead489470 \
-    --count 1 \
-    --instance-type t2.micro \
-    --key-name "$DB_KEY_NAME" \
-    --security-groups "$DB_SECURITY_GROUP" \
-    --iam-instance-profile Name=LabInstanceProfile \
-    --user-data file://db_ini.txt \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$DB_INSTANCE_NAME}]" > /dev/null
-
-# change the permissions for the ssh key to 600
-chmod 600 ~/.ssh/"$DB_KEY_NAME".pem
-
-# get the database instance id
-DB_INSTANCE_ID=$(aws ec2 describe-instances \
-    --filters "Name=tag:Name,Values=$DB_INSTANCE_NAME" \
-    --query "Reservations[0].Instances[0].InstanceId" \
-    --output text)
-
-# get the database instance public ip
-DB_PUBLIC_IP=$(aws ec2 describe-instances \
-    --instance-ids "$DB_INSTANCE_ID" \
-    --query "Reservations[0].Instances[0].PublicIpAddress" \
-    --output text)
-
-# print out the public ip
-echo -e "Das ist die Public-IP der Datenbank-Instanz: $DB_PUBLIC_IP"
+echo "======================================="
+echo " Nextcloud erreichbar unter:"
+echo " http://$NC_PUBLIC_IP"
+echo ""
+echo " DB Private IP:"
+echo " $DB_PRIVATE_IP"
+echo "======================================="
